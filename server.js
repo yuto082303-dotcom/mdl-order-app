2643639a4fc76b347914273a0e85ceaccdb3a4ae
yuto082303-dotcom <yuto082303@gmail.com>
2026-03-16 17:33:47 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const axios = require('axios');
 const app = express();
 
-app.get('*', (req, res) => {
-  res.redirect('https://script.google.com/macros/s/AKfycby5U4kMdU8PLde3A_BqVBESczEELqFN2alSHrBdKmTgTcYmVKDsvtX4_A3Wh0BzHcdBvA/exec');
+const GAS_URL = 'https://script.google.com/macros/s/AKfycby5U4kMdU8PLde3A_BqVBESczEELqFN2alSHrBdKmTgTcYmVKDsvtX4_A3Wh0BzHcdBvA/exec';
+
+app.get('*', async (req, res) => {
+  const response = await axios.get(GAS_URL + (req.url === '/' ? '' : req.url), { params: req.query });
+  res.send(response.data);
 });
 
 app.listen(process.env.PORT || 3000);
