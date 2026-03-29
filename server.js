9fa7d29211ff5f20268e278e8badbc0868adbbf0
yuto082303-dotcom <yuto082303@gmail.com>
2026-03-29 17:01:04 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 const GAS_URL = process.env.GAS_URL || '';
 app.use(express.json({ limit: '5mb' }));
 app.use(express.static('public'));
